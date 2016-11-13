cbb6d714b8324c5f46f9add75f03435ab50a5108
999mani <manisreekar.sr@gmail.com>
2016-11-13 19:16:40 +0530

    decoding done

--- decode.py
+++ decode.py
@@ -1,15 +1,16 @@
 from Crypto.Cipher import AES
 import base64
 import pyqrcode
-import qrtools
+from qrtools import QR
 
 
 path = input("Enter the path of the qr")
-qr = qrtools.QR()
-qr.decode(path)
-msg = qr.data
+qr = QR("qrimg.png")
+qr.decode()
+emsg = qr.data
+msg = emsg.rjust(16)
 key = input("Enter the secret key")
-secret_key = key
+secret_key = key.rjust(16)
 cipher = AES.new(secret_key,AES.MODE_ECB)
 decoded = cipher.decrypt(base64.b64decode(msg))
 print (decoded.strip())
