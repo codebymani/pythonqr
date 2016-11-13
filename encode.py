be7d2ce82167b126ada3b900f84b1ec1455f4c24
999mani <manisreekar.sr@gmail.com>
2016-11-13 14:56:04 +0530

    encoder encrypted with aes

--- encode.py
+++ encode.py
@@ -2,12 +2,12 @@ from Crypto.Cipher import AES
 import base64
 import pyqrcode
 
-//store your password in qr
+
 
 msg = 'passwords you want to store'.rjust(32)
-secret_key = 'my_secretkey123'
+secret_key = 'my_secretkey123'.rjust(16)
 cipher = AES.new(secret_key,AES.MODE_ECB)
 encode = base64.b64encode(cipher.encrypt(msg))
 qr = pyqrcode.create(encode)
-
+qr.png("qrimg.png",scale=20)
 
